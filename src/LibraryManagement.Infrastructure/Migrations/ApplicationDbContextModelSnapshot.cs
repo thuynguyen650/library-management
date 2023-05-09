c6a7b3bcd92c3b7593fd9edd52e43fed76b86a2d
nthuy <thuynguyen.webdev@gmail.com>
2023-05-09 21:30:41 +0700

    using assembly to apply all data config

--- src/LibraryManagement.Infrastructure/Migrations/ApplicationDbContextModelSnapshot.cs
+++ src/LibraryManagement.Infrastructure/Migrations/ApplicationDbContextModelSnapshot.cs
@@ -35,7 +35,7 @@ namespace LibraryManagement.Infrastructure.Migrations
 
                     b.HasKey("Id");
 
-                    b.ToTable("Books");
+                    b.ToTable("Books", (string)null);
                 });
 #pragma warning restore 612, 618
         }
--- src/LibraryManagement.Infrastructure/Persistence/ApplicationDbContext.cs
+++ src/LibraryManagement.Infrastructure/Persistence/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using LibraryManagement.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
+using System.Reflection;
 
 namespace LibraryManagement.Infrastructure.Persistence;
 
@@ -9,4 +10,31 @@ public class ApplicationDbContext : DbContext
     { }
 
     public DbSet<Book> Books { get; set; }
+
+    public DbSet<Author> Authors { get; set; }
+
+    public DbSet<BookAuthor> BookAuthors { get; set; }
+
+    public DbSet<BookCategory> BookCategories { get; set; }
+
+    public DbSet<BookCopy> BookCopies { get; set; }
+
+    public DbSet<Checkout> Checkouts { get; set; }
+
+    public DbSet<Hold> Holds { get; set; }
+
+    public DbSet<Notification> Notifications { get; set; }
+
+    public DbSet<Patron> Patrons { get; set; }
+
+    public DbSet<Publisher> Publishers { get; set; }
+
+    public DbSet<WaitingList> WaitingLists { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+        base.OnModelCreating(builder);
+    }
 }
